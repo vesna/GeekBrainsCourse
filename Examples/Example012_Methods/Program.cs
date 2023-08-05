98c11f3f3d2b6c7a333a37c3d8ce28b92bb66a01
Svetlana <svlight77@gmail.com>
2023-08-05 17:50:57 +0300

    add Example 012 Methods Console project

---
+++ Examples/Example012_Methods/Program.cs
@@ -0,0 +1,48 @@
+﻿//1
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+
+//2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст Сообщения");
+
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(count: 4, msg: "Текст Сообщения");
+
+//3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
+//4
+int Method4(int count, string c)
+{
+    int i = 0;
+    string result = string.Empty;
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "fwef");
+Console.WriteLine(res);
