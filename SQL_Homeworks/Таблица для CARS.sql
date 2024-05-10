dd4cd854a0f9839547391babf4f6653cb6bb6b3b
Svetlana <svlight77@gmail.com>
2024-05-11 00:41:25 +0300

    HW 5 sql

---
+++ SQL_Homeworks/Таблица для CARS.sql
@@ -0,0 +1,20 @@
+CREATE TABLE cars
+(
+	id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(45),
+    cost INT
+);
+
+INSERT cars
+VALUES
+	(1, 'Audi', 52642),
+    (2, 'Mercedes', 57127 ),
+    (3, 'Skoda', 9000 ),
+    (4, 'Volvo', 29000),
+	(5, 'Bentley', 350000),
+    (6, 'Citroen', 21000 ), 
+    (7, 'Hummer', 41400), 
+    (8, 'Volkswagen', 21600);
+    
+SELECT *
+FROM cars;
