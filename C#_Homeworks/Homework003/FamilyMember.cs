47a2845629a6c8442cf55c89c9dfef4e1601aaa1
Svetlana <svlight77@gmail.com>
2024-06-13 15:44:53 +0300

    FamilyMember class C#

---
+++ C#_Homeworks/Homework003/FamilyMember.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework003
+{
+    internal class FamilyMember
+    {
+        public string Name { get; set; }
+        public FamilyMemberType MemberType { get; set; }
+        public FamilyMember? Mother { get; set; }
+        public FamilyMember? Father { get; set; }
+        public FamilyMember? Partner { get; set; }
+        public FamilyMember[]? Children { get; set; }        
+        public FamilyMember(string name, FamilyMemberType familyMemberType) {
+            this.Name = name;
+            this.MemberType = familyMemberType;
+        }
+
+        public string CloseRelevants()
+        {
+            string result = string.Empty;
+            
+            if (Partner == null && Children == null)
+                result = "Нет близких родственников";
+            else
+            {
+                StringBuilder sb = new StringBuilder("Близкие родственники ");
+                sb.Append(Name);
+                sb.Append('\n');
+                if (Partner != null)
+                {
+                    if (MemberType == FamilyMemberType.Man)
+                        sb.Append("Муж: ");
+                    else
+                        sb.Append("Жена: ");
+                    sb.Append(Partner.Name);
+                    sb.Append('\n');
+                }
+                if (Children != null)
+                {
+                    sb.Append("Дети: ");
+                    foreach (var child in Children)
+                    {
+                        sb.Append(child.Name);
+                        sb.Append(' ');
+                    }
+                }
+                result = sb.ToString();
+            }
+            
+            return result;
+        }
+    }
+
+    public enum FamilyMemberType { Man, Woman }
+}
---
+++ C#_Homeworks/Homework003/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Урок 1. Классы и ООП
+Доработайте приложение генеалогического дерева таким образом чтобы программа выводила на экран близких родственников (жену/мужа). 
+Продумайте способ более красивого вывода с использованием горизонтальных и вертикальных черточек.
+*/
+using Homework003;
+
+public class Programm
+{
+    public static void Main(string[] args)
+    {
+        FamilyMember m0 = new FamilyMember("M0", FamilyMemberType.Man);
+        FamilyMember m1 = new FamilyMember("M1", FamilyMemberType.Man);
+        FamilyMember m2 = new FamilyMember("M2", FamilyMemberType.Woman);
+        m1.Partner = m2;
+        m2.Partner = m1;
+        m1.Children = new FamilyMember[] { m0 };
+        m2.Children = new FamilyMember[] { m0 };
+
+        Console.WriteLine(m1.CloseRelevants());
+
+    }
+}
