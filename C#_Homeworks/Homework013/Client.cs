5c6bc687c7efa21bda742e048cf903bc86d822cc
Svetlana <svlight77@gmail.com>
2024-06-26 21:23:53 +0300

    Chat wirh Exit function

---
+++ C#_Homeworks/Homework013/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework013
+{
+    internal class Client
+    {
+        public static void SendMsg(string nikname)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            UdpClient udpClient = new UdpClient();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите сообщение");
+                    var text = Console.ReadLine();
+                    if (String.IsNullOrEmpty(text))
+                    {
+                        break;
+                    }
+                    
+                    var msg = new Message(nikname, text);
+                    var json = msg.ToJson();
+                    var data = Encoding.UTF8.GetBytes(json);
+                    udpClient?.Send(data, iPEndPoint);
+
+                    byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                    string str1 = Encoding.UTF8.GetString(buffer);
+                    var somemessage = Message.FromJson(str1);
+                    Console.WriteLine(somemessage?.ToString());
+                    if (text == "Exit") {
+                        udpClient.Close();
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    udpClient?.Close();
+                    Console.WriteLine($"Что-то пошло не так: {e.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ C#_Homeworks/Homework013/Server.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Homework013
+{
+    internal class Server
+    {
+        public static void AcceptMsg()
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint((IPAddress)IPAddress.Any, 0);
+            UdpClient udpClient = new UdpClient(12345);
+            Console.WriteLine("Сервер ожидает сообщения отклиента");
+
+            while (true)
+            {
+                string text = "";
+                try
+                {
+                    byte[]? buffer = udpClient?.Receive(ref iPEndPoint);
+                    string data = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+                    Thread tr = new Thread(() =>
+                    {
+                        Message? msg = Message.FromJson(data);
+                        if (msg != null)
+                        {
+                            text = msg.Text;
+
+                            Console.WriteLine(msg?.ToString());
+                            var newMsg = new Message("Server", "Сообщение получено");
+                            var json = newMsg.ToJson();
+                            var bytes = Encoding.UTF8.GetBytes(json);
+                            udpClient?.Send(bytes, iPEndPoint);
+                        }
+                        else { Console.WriteLine("Сообщение не корректно"); }
+                    });
+                    tr.Start();
+                    tr.Join();
+                    if (text == "Exit")
+                    {
+                        tr.Interrupt();
+                        udpClient?.Close();
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    udpClient?.Close();
+                    Console.WriteLine($"Что-то пошло не так: {e.Message}");
+                }
+            }
+        }
+    }
+}
