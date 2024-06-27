116e67ac974b1f6b25ea69dce536007079ae6722
Svetlana <svlight77@gmail.com>
2024-06-28 02:55:26 +0300

    Chat wirh Exit function by token

---
+++ C#_Homeworks/Homework014/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework014
+{
+    internal class Client
+    {
+        public static async Task SendMsg(string nikname)
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 12345);
+            UdpClient udpClient = new UdpClient();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите сообщение");
+                    var text = Console.ReadLine();
+                    if (String.IsNullOrEmpty(text) || text.ToLower() == "exit")
+                    {
+                        break;
+                    }
+
+                    var msg = new Message(nikname, text);
+                    var json = msg.ToJson();
+                    var data = Encoding.UTF8.GetBytes(json);
+                    var t = await udpClient.SendAsync(data, iPEndPoint);
+
+                    var q = await udpClient.ReceiveAsync();
+                    byte[] buffer = q.Buffer;//udpClient.Receive(ref iPEndPoint);
+                    string str1 = Encoding.UTF8.GetString(buffer);
+                    var somemessage = Message.FromJson(str1);
+                    Console.WriteLine(somemessage?.ToString());
+                }
+                catch (Exception e)
+                {
+                    udpClient?.Close();
+                    Console.WriteLine($"Что-то пошло не так: {e.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ C#_Homeworks/Homework014/Server.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Homework014
+{
+    internal class Server
+    {
+        static private CancellationTokenSource cts = new CancellationTokenSource();
+
+        public static async Task AcceptMsg()
+        {
+            IPEndPoint iPEndPoint = new IPEndPoint((IPAddress)IPAddress.Any, 0);
+            UdpClient udpClient = new UdpClient(12345);
+            Console.WriteLine("Сервер ожидает сообщения отклиента");
+
+            Task cancelTask = Task.Run(() => {
+                var temp = Console.ReadKey(true);
+
+                Console.WriteLine($"\nнажата {temp.Key}: завершеие работы.\n");
+                cts.Cancel();
+            });
+
+
+            while (true)
+            {
+                try
+                {
+                    byte[] buffer = udpClient.Receive(ref iPEndPoint); 
+                    string data = Encoding.UTF8.GetString(buffer, 0, buffer.Length);
+
+                    var serverTask = Task.Run(async () =>
+                    {
+                        if (cts.IsCancellationRequested)  // проверяем наличие сигнала отмены задачи
+                        {
+                            Console.WriteLine("Операция прервана");
+                            return;     //  выходим из метода и тем самым завершаем задачу
+                        }
+                        Message? msg = Message.FromJson(data);
+                        if (msg != null)
+                        {
+                            Console.WriteLine(msg?.ToString());
+                            var newMsg = new Message("Server", "Сообщение получено");
+                            var json = newMsg.ToJson();
+                            var bytes = Encoding.UTF8.GetBytes(json);
+                            await udpClient.SendAsync(bytes, iPEndPoint);
+                        }
+                        else { Console.WriteLine("Сообщение не корректно"); }
+
+                    }, cts.Token);
+
+                }
+                catch (Exception e)
+                {
+                    udpClient?.Close();
+                    Console.WriteLine($"Что-то пошло не так: {e.Message}");
+                }
+            }
+        }
+    }
+}
